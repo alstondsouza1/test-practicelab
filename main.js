9999167286c416fee23cdb8f098b348fb4899c95
alstondsouza1 <alstondsouza35@gmail.com>
2025-01-12 23:59:30 -0800

    added files to gitignore

---
+++ main.js
@@ -0,0 +1,15 @@
+import lodash from 'lodash';
+import chalk from 'chalk';
+
+const array = [1, 4, 2, 3, 2, 1, 4, 5, 5, 2, 3, 2, 1];
+const uniqueNums = lodash.uniq(array);
+console.log(uniqueNums);
+
+console.log(chalk.yellow(uniqueNums));
+
+console.log(chalk.bgGray(uniqueNums));
+
+console.log(chalk.red(uniqueNums));
+
+
+console.log(chalk.blueBright("Hello, with chalk!"))
